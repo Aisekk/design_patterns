35e9f5b9b9617f3a6ab1b39e5daeb5fd35e597be
Alexander Chekmarev <gooletters@gmail.com>
2022-10-27 15:04:05 +0300

    ObjectPool.h

--- project/project/main.cpp
+++ project/project/main.cpp
@@ -2,13 +2,15 @@
 #include "Command.h"
 #include "Flyweight.h"
 #include "Flyweight_CppRef.h"
+#include "ObjectPool.h"
 using namespace std;
 
 int main()
 {
     //command();
     //flyweight();
-    flyweight_cppref();
+    //flyweight_cppref();
+    objectPool();
 
     system("pause");
 	return 0;
